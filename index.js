c742fcd9d18f15a718260002b7df1ce511af2244
Gisele Cesar <61174544+gisele-cesar@users.noreply.github.com>
2020-07-07 17:05:32 -0300

    feita function que devolve array de objs

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+
+
+module.exports = (path) => {
+  // const regex = /\[(.*)\]\((http.*)\)/gm;
+  // console.log(regex);
+  let arrLinks = [];
+  let obj = [];
+
+  fs.readFile(path, 'utf8', function (err, data) {
+    if (err) {
+      (`erro de leitura de arquivo ${err}`);
+    } else {
+      arrLinks = data.match(/\[(.*)\]\((ht.*)\)/gm);
+      arrLinks.forEach(link => {
+        obj.push({
+          text: `${link.match(/(?<=\[).+?(?=\])/g)}`,
+          href: `${link.match(/(?<=\().+?(?=\))/g)}`,
+          file: path.replace('[]', '')
+        })
+        console.log(obj);
+      })
+    }
+    //Enviando para o console o resultado da leitura
+    // console.log(data);
+  });
 };
+
+module.exports('README.md');
