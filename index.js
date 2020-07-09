3d888463953ba06402f24b8f0df1363a15a7610e
Gisele Cesar <61174544+gisele-cesar@users.noreply.github.com>
2020-07-09 17:29:38 -0300

    retirado comentários de index.js

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-/* const directoryPath = path.join(__dirname, "files");
-
-fs.readdir(directoryPath, function (err, files) {
-  if (err) {
-    console.log("Error getting directory information.")
-  } else {
-    const filesMd = files.filter(function (file) {
-      return path.extname(file === '.md')
-    })
-    filesMd.forEach(function (file) {
-      console.log(file)
-    })
-  }
-}); */
-
 const mdLinks = (path) => {
-  // const regex = /\[(.*)\]\((http.*)\)/gm;
-  // console.log(regex);
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8', function (err, data) {
       if (err) {
         return reject(`File not found`);
-        // (`erro de leitura de arquivo ${err}`);
       } else {
         const regexLinks = data.match(/\[(.*)\]\((ht.*)\)/gm);
         let array = [];
@@ -34,13 +16,11 @@ const mdLinks = (path) => {
             file: path.replace('[]', '')
           };
           array.push(obj);
-          // console.log(obj);
         })
         return resolve(array);
       }
     });
   })
-
 };
 
 module.exports = mdLinks;
